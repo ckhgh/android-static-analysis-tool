9210ec307b2ac0603656d037948fab7eed0970a8
Samuel Chan <chankaihei@gmail.com>
2026-04-10 09:22:04 +0800

    feat: renamed files

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,103 +1,79 @@
-import hashlib
-import androguard.util
-
-androguard.util.set_log("CRITICAL")
-
-
-def calculate_file_hash(apk_path):
-    sha256_hash = hashlib.sha256()
-    with open(apk_path, "rb") as f:
-        for byte_block in iter(lambda: f.read(4096), b""):
-            sha256_hash.update(byte_block)
-    return sha256_hash.hexdigest()
-
-
-def extract_manifest(apk):
-    permissions = apk.get_permissions()
-    activities = apk.get_activities()
-    services = apk.get_services()
-    receivers = apk.get_receivers()
-
-    return {
-        "permissions": permissions,
-        "activities": activities,
-        "services": services,
-        "receivers": receivers,
-    }
+from androguard.core.apk import APK
+from androguard.misc import AnalyzeAPK
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm import tqdm
+import json
+import os
+import multiprocessing
+from feature_extraction import (
+    calculate_file_hash,
+    extract_manifest,
+    extract_intent_filters,
+    extract_apis,
+    extract_opcodes,
+)
 
 
-def extract_intent_filters(apk):
-    intent_filters = {"activities": {}, "services": {}, "receivers": {}}
+def analyze_single_apk(apk_path: str, output_folder: str) -> None:
 
-    for activity in apk.get_activities():
-        filters = apk.get_intent_filters("activity", activity)
-        intent_filters["activities"][activity] = filters
+    filename = os.path.basename(apk_path)
+    output_file = os.path.join(output_folder, filename + "_analysis.json")
 
-    for service in apk.get_services():
-        filters = apk.get_intent_filters("service", service)
-        intent_filters["services"][service] = filters
+    if os.path.exists(output_file):
+        print(f"Skipping {filename}")
+        return
 
-    for receiver in apk.get_receivers():
-        filters = apk.get_intent_filters("receiver", receiver)
-        intent_filters["receivers"][receiver] = filters
+    try:
+        a, d, dx = AnalyzeAPK(apk_path)
+        apis = extract_apis(dx)
+        opcodes = extract_opcodes(dx)
+        file_hash = calculate_file_hash(apk_path)
+        manifest = extract_manifest(a)
+        intent_filters = extract_intent_filters(a)
 
-    return intent_filters
+        extracted_features = {
+            "hash": file_hash,
+            "manifest": manifest,
+            "intentFilters": intent_filters,
+            "apiCalls": apis,
+            "opcodes": opcodes,
+        }
 
+        with open(output_file, "w") as f:
+            json.dump(extracted_features, f, indent=4)
 
-def extract_apis(dx):
-    api_calls = {}
-
-    for method in dx.get_methods():
-        if method.is_external():
-            xrefs = method.get_xref_from()
-            if xrefs:
-                api_name = f"{method.class_name}->{method.name}{method.descriptor}"
-                callers = []
-                for _, caller_method, _ in xrefs:
-                    callers.append(caller_method.full_name)
-
-                api_calls[api_name] = callers
-
-    return api_calls
+    except Exception as e:
+        print(f"Error analyzing {filename} {e}")
 
 
-def extract_opcodes(dx):
-    opcodes = {}
-    for method in dx.get_methods():
-        if method.is_external():
-            continue
-        m = method.get_method()
-        if m and m.get_code():
-            for ins in m.get_instructions():
-                op_name = ins.get_name()
-                opcodes[op_name] = opcodes.get(op_name, 0) + 1
-    return opcodes
+if __name__ == "__main__":
+    folder_pairs = [("Malicious", "MaliciousExtracted"), ("Benign", "BenignExtracted")]
 
+    for input_folder, output_folder in folder_pairs:
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
-def extract_features(data: dict) -> dict:
-    feats = {}
-    manifest = data.get("manifest", {})
-    for perm in manifest.get("permissions", []):
-        feats[f"perm_{perm}"] = 1.0
-    for act in manifest.get("activities", []):
-        feats[f"act_{act}"] = 1.0
-    for srv in manifest.get("services", []):
-        feats[f"srv_{srv}"] = 1.0
-    for rec in manifest.get("receivers", []):
-        feats[f"rec_{rec}"] = 1.0
+        apk_files = [
+            os.path.join(input_folder, f)
+            for f in os.listdir(input_folder)
+            if f.endswith(".apk") and os.path.isfile(os.path.join(input_folder, f))
+        ]
 
-    intent_filters = data.get("intentFilters", {})
-    for comp_type, comp_dict in intent_filters.items():
-        for comp_name, filters in comp_dict.items():
-            for action in filters.get("action", []):
-                feats[f"intent_action_{action}"] = 1.0
-            for category in filters.get("category", []):
-                feats[f"intent_category_{category}"] = 1.0
+        print(f"\nProcessing {input_folder} folder with{len(apk_files)} APKs ")
 
-    for api_call in data.get("apiCalls", {}).keys():
-        feats[f"api_{api_call}"] = 1.0
+        max_workers = multiprocessing.cpu_count() - 5
 
-    for op_name, count in data.get("opcodes", {}).items():
-        feats[f"opcode_{op_name}"] = float(count)
+        print(f"parallel process with {max_workers} core")
 
-    return feats
+        with ProcessPoolExecutor(max_workers=max_workers) as executor:
+            futures = [executor.submit(analyze_single_apk, apk, output_folder) for apk in apk_files]
+
+            for future in tqdm(
+                as_completed(futures),
+                total=len(apk_files),
+                desc=f"Analyzing {input_folder}",
+                unit="apk",
+            ):
+                future.result()
+
+    print("\nDone")
---
+++ static_analysis_tool.py
@@ -0,0 +1,153 @@
+import os
+import json
+import numpy as np
+from scipy import sparse
+import joblib
+from tqdm import tqdm
+from androguard.misc import AnalyzeAPK
+from feature_extraction import (
+    calculate_file_hash,
+    extract_manifest,
+    extract_intent_filters,
+    extract_apis,
+    extract_opcodes,
+    extract_features
+)
+
+
+APK_DIR = "StaticAnalysis"
+EXTRACTED_DIR = "StaticAnalysisExtracted"
+SPARSE_DIR = "SparseMatrix"
+MODEL_PATH = "TrainedModels/xgboost.joblib"
+MALICIOUS_THRESHOLD = 0.70
+SUSPICIOUS_THRESHOLD = 0.30
+
+
+#loading the model and features name correspond to the 400 features used to train XGB
+model = joblib.load(MODEL_PATH)
+with open(os.path.join(SPARSE_DIR, "feature_names.json"), "r", encoding="utf-8") as f:
+    final_feature_names = json.load(f)
+feature_to_col = {name: idx for idx, name in enumerate(final_feature_names)}
+
+
+# Ensure the extracted directory exists
+os.makedirs(EXTRACTED_DIR, exist_ok=True)
+
+
+#same feature extraction function as stage 1
+def analyze_single_apk(apk_path: str):
+    filename = os.path.basename(apk_path)
+    output_file = os.path.join(EXTRACTED_DIR, filename + "_analysis.json")
+
+    if os.path.exists(output_file):
+        print(f"Skipping {filename}")
+        return output_file
+
+    try:
+        a, d, dx = AnalyzeAPK(apk_path)
+        extracted_features = {
+            "hash": calculate_file_hash(apk_path),
+            "manifest": extract_manifest(a),
+            "intentFilters": extract_intent_filters(a),
+            "apiCalls": extract_apis(dx),
+            "opcodes": extract_opcodes(dx),
+        }
+        with open(output_file, "w", encoding="utf-8") as f:
+            json.dump(extracted_features, f, indent=4, ensure_ascii=False)
+    except Exception as e:
+        print(f"Error analyzing {filename} {e}")
+        return output_file
+
+
+# get list of all apk
+apk_files = [
+    os.path.join(APK_DIR, f)
+    for f in os.listdir(APK_DIR)
+    if f.lower().endswith(".apk") and os.path.isfile(os.path.join(APK_DIR, f))
+]
+
+
+#same feature extraction to JSON as stage 1
+analysis_files = []
+for apk_path in tqdm(apk_files, desc=f"Extracting APKs", unit="apk"):
+    json_path = analyze_single_apk(apk_path)
+    if json_path:
+        analysis_files.append(json_path)
+
+
+#load the JSON for classification
+results = []
+for json_path in tqdm(analysis_files, desc="Analyzing and classifying APKs with XGBoost", unit="apk"):
+    try:
+        with open(json_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        #Build sparse matrix with the same 400 features as stage 1.5
+        feat_dict = extract_features(data)
+        vector = np.zeros(len(final_feature_names), dtype=np.float32)
+        for feat_name, value in feat_dict.items():
+            if feat_name in feature_to_col:
+                vector[feature_to_col[feat_name]] = value
+        X_new = sparse.csr_matrix([vector])
+
+        # calculate classification score
+        proba = model.predict_proba(X_new)[0][1]
+
+        #save apk name and probability (we determine label later based on confidence)
+        apk_name = os.path.basename(json_path).replace("_analysis.json", "")
+        results.append({
+            "apk": apk_name,
+            "malicious_prob": float(proba)
+        })
+    except Exception as e:
+        print(f"Error classifying {os.path.basename(json_path)}: {e}")
+
+
+#print results
+print("\n" + "="*85)
+print(" " * 28 + "CLASSIFICATION RESULTS")
+print("="*85)
+
+safe_count = 0
+suspicious_count = 0
+malicious_count = 0
+
+for r in results:
+    proba = r["malicious_prob"]
+    prob_percent = proba * 100
+
+    if proba >= MALICIOUS_THRESHOLD:
+        label = "Malicious"
+        rec_action = "Avoid installing this APK and consider deleting it."
+        malicious_count += 1
+    elif proba >= SUSPICIOUS_THRESHOLD:
+        label = "Suspicious"
+        rec_action = "Further analysis or testing is recommended before installing the APK."
+        suspicious_count += 1
+    else:
+        label = "Safe"
+        rec_action = "The APK is safe to install."
+        safe_count += 1
+
+
+    # add color to the label
+    color = {
+        "Malicious": "\033[91m",
+        "Suspicious": "\033[93m",
+        "Safe": "\033[92m"
+    }
+    status = f"{color[label]}{label}\033[0m"
+
+
+    print(f"APK                    : {r['apk']}")
+    print(f"Result                 : {status}")
+    print(f"Malicious probability  : {prob_percent:.1f}%")
+    print(f"Recommended action     : {rec_action}")
+    print("-" * 85)
+
+print("="*85)
+print(f"Total APKs processed : {len(results)}")
+print(f"Benign count         : {safe_count}")
+print(f"Suspicious count     : {suspicious_count}")
+print(f"Malicious count      : {malicious_count}")
+print("="*85)
